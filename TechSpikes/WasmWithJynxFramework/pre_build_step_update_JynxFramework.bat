1d55e3a7ad9d71007cebb8753f72cd134245d22b
Jonathan Markland <jonathan.markland@btinternet.com>
2022-01-15 23:32:03 +0000

    Spike the building of the JynxFramework with Clang.

---
+++ TechSpikes/WasmWithJynxFramework/pre_build_step_update_JynxFramework.bat
@@ -0,0 +1,3 @@
+@REM Copies the JynxFramework.h and cpp files from the development repository
+copy /Y ..\..\..\JynxFramework\JynxFramework\JynxFramework.h .
+copy /Y ..\..\..\JynxFramework\JynxFramework\JynxFramework.cpp .
