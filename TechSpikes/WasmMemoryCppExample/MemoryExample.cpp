26154df67d3ec1183634adb3e4b0ea38e595e510
Jonathan Markland <jonathan.markland@btinternet.com>
2022-01-15 17:49:42 +0000

    Rename to spike folder to indicate we're invoking clang in C++ mode for the first time.

--- TechSpikes/WasmMemoryCppExample/MemoryExample.cpp
+++ TechSpikes/WasmMemoryCppExample/MemoryExample.cpp
@@ -1,23 +1,23 @@
 
-// clang -O2 --target=wasm32 --no-standard-libraries -Wl,--export-all -Wl,--no-entry -o MemoryExample.wasm MemoryExample.c
+// clang --std=c++17 -O2 --target=wasm32 --no-standard-libraries -Wl,--export-all -Wl,--no-entry -o MemoryExample.wasm MemoryExample.c
 
 extern unsigned char __heap_base;
 
 __attribute__ ((visibility("default"))) 
-void *get_heap_base(void) 
+extern "C" void *get_heap_base(void) 
 {
 	return &__heap_base;
 }
 
 void *MyHeap = (void *) 0;
 
-int jmalign(int size)
+extern "C" int jmalign(int size)
 {
 	if (size & 3) return (size | 3) + 1;
 	return size;
 }
 
-void *jmmalloc(int size)
+extern "C" void *jmmalloc(int size)
 {
 	void *block = MyHeap;
 	if (block == (void *) 0)
@@ -29,7 +29,7 @@ void *jmmalloc(int size)
 	return block;
 }
 
-void *create_block()
+extern "C" void *create_block()
 {
 	return jmmalloc(10);
 }
