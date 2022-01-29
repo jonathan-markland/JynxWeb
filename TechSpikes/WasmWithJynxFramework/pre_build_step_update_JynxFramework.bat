e8f842ee22bccb61842587cf3637d2a7c7233f7a
Jonathan Markland <jonathan.markland@btinternet.com>
2022-01-29 10:13:40 +0000

    Update batch file to copy Jynx Panic files.

--- TechSpikes/WasmWithJynxFramework/pre_build_step_update_JynxFramework.bat
+++ TechSpikes/WasmWithJynxFramework/pre_build_step_update_JynxFramework.bat
@@ -1,3 +1,5 @@
 @REM Copies the JynxFramework.h and cpp files from the development repository
+copy /Y ..\..\..\JynxFramework\JynxFramework\JynxFrameworkPanic.h .
+copy /Y ..\..\..\JynxFramework\JynxFramework\JynxFrameworkPanic.cpp .
 copy /Y ..\..\..\JynxFramework\JynxFramework\JynxFramework.h .
 copy /Y ..\..\..\JynxFramework\JynxFramework\JynxFramework.cpp .
