53e22bf0575f56ed003c39256078acc5e4d62d68
Jonathan Markland <jonathan.markland@btinternet.com>
2022-05-04 14:23:39 +0100

    Sound

--- TechSpikes/SDL2DesktopSpike/SDL2DesktopSpike.cpp
+++ TechSpikes/SDL2DesktopSpike/SDL2DesktopSpike.cpp
@@ -4,16 +4,56 @@
 #include "SDL2/SDL_image.h"
 #include "SDL2/SDL_timer.h"
 
+
+void MyAudioCallback(void *userdata, Uint8 *stream, int len)
+{
+    /*
+        userdata
+        an application-specific parameter saved in the SDL_AudioSpec structure's userdata field
+
+        stream
+        a pointer to the audio data buffer filled in by SDL_AudioCallback()
+
+        len
+        the length of that buffer in bytes
+    */
+
+    auto floatStream = (float*)stream;
+
+    int floatCount = len / sizeof(float);
+
+    auto halfWay   = floatStream + (floatCount / 2);
+    auto endBuffer = floatStream + floatCount;
+
+    for (auto p = floatStream; p < halfWay; p++)
+    {
+        *p = -0.3F;
+    }
+    for (auto p = halfWay; p < endBuffer; p++)
+    {
+        *p = +0.3F;
+    }
+}
+
+
+
+const int WindowWidth = 500;
+const int WindowHeight = 500;
+
+
+
 int main(int argc, char* argv[])
 {
     // returns zero on success else non-zero
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("error initializing SDL: %s\n", SDL_GetError());
+        return 1;
     }
+
     SDL_Window* win = SDL_CreateWindow("GAME", // creates a window
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        1000, 1000, 0);
+        WindowWidth, WindowHeight, 0);
 
     // triggers the program that controls
     // your graphics hardware and sets flags
@@ -46,16 +86,50 @@ int main(int argc, char* argv[])
     dest.h /= 2;
 
     // sets initial x-position of object
-    dest.x = (1000 - dest.w) / 2;
+    dest.x = (WindowWidth - dest.w) / 2;
 
     // sets initial y-position of object
-    dest.y = (1000 - dest.h) / 2;
+    dest.y = (WindowHeight - dest.h) / 2;
 
     // controls animation loop
     int close = 0;
 
     // speed of box
     int speed = 300;
+
+    // -----------------------------------------------------------------------------------
+
+    SDL_AudioSpec want;
+    SDL_memset(&want, 0, sizeof(want)); /* or SDL_zero(want) */
+    want.freq = 48000;
+    want.format = AUDIO_F32;
+    want.channels = 2;
+    want.samples = 512;
+    want.callback = MyAudioCallback; /* you wrote this function elsewhere -- see SDL_AudioSpec for details */
+
+    SDL_AudioSpec have;
+
+    SDL_AudioDeviceID dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
+    if (dev != 0) 
+    {
+        if (have.format != want.format) 
+        {
+            /* we let this one thing change. */
+            printf("We didn't get Float32 audio format.");
+            return 1;
+        }
+
+        SDL_PauseAudioDevice(dev, 0); /* start audio playing. */
+        SDL_Delay(5000); /* let the audio callback play some sound for 5 seconds. */
+        SDL_CloseAudioDevice(dev);
+    }
+    else
+    {
+        printf("SDL_OpenAudioDevice() failed");
+        return 1;
+    }
+
+    // -----------------------------------------------------------------------------------
 
     // animation loop
     while (!close) {
@@ -97,7 +171,7 @@ int main(int argc, char* argv[])
 
         // right boundary
         if (dest.x + dest.w > 1000)
-            dest.x = 1000 - dest.w;
+            dest.x = WindowWidth - dest.w;
 
         // left boundary
         if (dest.x < 0)
@@ -105,7 +179,7 @@ int main(int argc, char* argv[])
 
         // bottom boundary
         if (dest.y + dest.h > 1000)
-            dest.y = 1000 - dest.h;
+            dest.y = WindowHeight - dest.h;
 
         // upper boundary
         if (dest.y < 0)
